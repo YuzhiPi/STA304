9d184eb93f2c163860753a2e76b5b1ac97d33adc
Yuzhi Pi <sherrypi@Sherrys-MacBook-Pro.local>
2022-02-06 18:42:03 -0500

    hi

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-# starter_folder
+# Overview
+This project aims to analyze the trend in the homicide cases in Toronto. The dataset used for this project is extracted from
+Open Data Toronto; cleaned and analyzed using R. The datase includes all reported homicide cases from 2004 to 2020.
 
-This repo....
+This project focuses on three aspects of the homicide cases - the general trend of annual case number over the 17 years time span, the 
+trend in its occurrence, observed by months and geographical location (represented by Toronto Police District), and the types of homicide. 
 
-It is organised as follows...
+By investigating the trend in occurrence, I believe that the government will be able to better allocate its resource to prevent and react 
+to homicide crimes in Toronto. Correponding policies can also be placed and reinforced based on the result. 
+
+# R Packages
+In order to process the data, these packages will have to be installed: 
+  1) opendatatoronto
+  2) dplyr
+  3) tidyverse
+  4) kableExtra
+  
+# File Structure
+Under folder "input", you will be able to see the literatures referenced in this project
+Under folder "output" -> "paper", you will be able to find the original RMD, reference list, and the final PDF of the report
+Under folder "script", you will be able to find the R code that cleans and plot the graphs and tables in the project
---
+++ scripts/RscriptAss1.R
@@ -0,0 +1,105 @@
+
+
+#Load package needed for this assignment
+library(opendatatoronto)
+library(dplyr)
+library(tidyverse)
+library(kableExtra)
+
+# Below is copied from https://open.toronto.ca/dataset/police-annual-statistical-report-homicide/ under "for developers"
+
+# get package
+package <- show_package("7d72bbbe-8adc-4b36-8ad1-5359f1c7a9cc")
+
+
+# get all resources for this package
+resources <- list_package_resources("7d72bbbe-8adc-4b36-8ad1-5359f1c7a9cc")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+
+data <- tibble(
+  Event_Unique_Id = data$Event_Unique_Id,
+  Occurrence_year = data$Occurrence_year,
+  Division = data$Division,
+  Homicide_Type = data$Homicide_Type,
+  Occurrence_Date = data$Occurrence_Date,
+  Hood_ID = data$Hood_ID,
+  Neighbourhood = data$Neighbourhood,
+  ObjectId = data$ObjectId
+)
+
+class(data)
+
+glimpse(data)
+```
+
+# Data cleaning 
+cleaned_data <- 
+  data %>%
+  select(Occurrence_year, Division, Homicide_Type, Occurrence_Date, Hood_ID, Neighbourhood,)
+# Remove coloums that are not needed
+
+cleaned_data <-
+  cleaned_data %>%
+  separate(col = Occurrence_Date,
+           into = c('year', 'month', 'other'),
+           sep = '-')
+# Obtain month data by separating Occurance_date, remove column "other"
+
+cleaned_data <- cleaned_data[-c(6)]
+
+glimpse(cleaned_data)
+
+#Trend of Count of Homicide by Year
+cleaned_data %>% ggplot(aes(x= Occurrence_year)) + geom_bar() + theme_classic() +
+  labs(x = "Occurence Year", y = "Frequency", title = "Figure 1: Summmary of Homicide Occurrence Year")+
+  geom_text(stat = 'count', aes(label = ..count..,vjust = -0.5))
+
+#Trend of Count of Homicide by month
+cleaned_data %>% ggplot(aes(x= month)) + geom_bar() + theme_classic() +
+  labs(x = "Occurrence Month", y = "Case Count", title = "Figure 2: Summmary of Homicide Occurrence Month")+
+  geom_text(stat = 'count', aes(label = ..count..,vjust = -0.5))
+
+# Geometric Location
+cleaned_data %>% ggplot(aes(x= Division)) + geom_bar(fill = "grey") + theme_classic() +
+  labs(x = "Police Division", y = "Count", title = "Figure 3: Summmary of Homicide Occurrence Location") +
+  coord_flip()+
+  geom_text(stat = 'count', aes(label = ..count..,vjust = 0.5))
+
+# Aggregate different homicide type by year
+tableprep <- cleaned_data %>% 
+  select(Occurrence_year, Homicide_Type) %>%
+  group_by(Occurrence_year) %>% 
+  count(Homicide_Type) %>% 
+  ungroup() %>% 
+  group_by(Occurrence_year) %>% 
+  mutate(percentage = paste(round(100 * (n /sum(n)), 1), "%"))
+
+knitr::kable(tableprep, caption = "Summary Table of Homicide Type and Percentage","pipe", align = c("l", "l", "c", "c"), col.names = c("Occurance Year", "Homicide Type", "Number of Cases Per Year", "% of Total Case For the Year"))
+
+
+#Ready the data for plotting stacked line graph
+graphprep <- cleaned_data %>% 
+  select(Occurrence_year, Homicide_Type) %>%
+  group_by(Occurrence_year) %>% 
+  count(Homicide_Type) %>% 
+  ungroup() %>% 
+  group_by(Occurrence_year) %>% 
+  mutate(percentage = round(100 * (n /sum(n)), 1))
+
+#Plot Stacked Line for count
+ggplot(graphprep,aes(x = Occurrence_year, y = n, color = Homicide_Type, group = Homicide_Type))+
+  geom_line() + 
+  geom_point() +
+  labs(x = "Occurrence Year", y = "Number of Cases", title = "Figure 4: Count of Different Homicide Type" )
+
+
+#Plot Stacked Line for percentage
+ggplot(graphprep,aes(x = Occurrence_year, y = percentage, color = Homicide_Type, group = Homicide_Type))+
+  geom_line() + 
+  geom_point() +
+  labs(x = "Occurrence Year", y = "Percentage", title = "Figure 5: Percentage of Different Homicide Type")
